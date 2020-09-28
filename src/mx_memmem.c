c7980aae991a7b754a28b988fb9f6097f036fdba
Alina Mazur <amazur@e1r4p16.unit.ua>
2020-09-28 22:26:10 +0300

    seems like fixed memmem, have to test it

--- src/mx_memmem.c
+++ src/mx_memmem.c
@@ -1,23 +1,21 @@
 #include "libmx.h"
-// TODO mak iets met dit funct! het werkt niet
+// TODO seems like fixed it, have to test well
 
-void *mx_memmem(const void *big,
-                size_t big_len,
-                const void *little,
-                size_t little_len
-                ) {
-    unsigned char *uc_big = (unsigned char*)big;
-    unsigned char *uc_lil = (unsigned char*)little;
+void *mx_memmem(const void *big, size_t big_len, const void *little,
+                size_t little_len) {
+    unsigned char *uc_big = (unsigned char *)big;
+    unsigned char *uc_lil = (unsigned char *)little;
     int cmp_rslt = 0;
 
     for (size_t i = 0; i < big_len && uc_big[i]; i++) {
         if (*(uc_big + i) == *uc_lil) {
             cmp_rslt = mx_memcmp((uc_big + i), uc_lil, little_len);
-        }
-        if (cmp_rslt == 0) {
-            return (void*)(uc_big + i);
+
+            if (cmp_rslt == 0) {
+                return (void *)(uc_big + i);
+            }
         }
     }
 
-    return (void*)NULL;
+    return (void *)NULL;
 }
